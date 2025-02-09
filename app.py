3aa4782e470bd23b84e584cd7e4fc4ffd2765365
venajustin <venajustin@gmail.com>
2025-02-09 00:05:16 -0800

    working on getting app as a route

--- app.py
+++ app.py
@@ -36,6 +36,60 @@ def reload_app():
     else:
         return jenv.get_template('error.html').render(errmsg = "ERROR OCCURED ON BACKEND")
 
+@app.route('/api/container/<containerid>/select', methods=['GET'])
+def select_container(containerid):
+   # select(containerid) # todo: user auth and select for user
+
+@app.route('/api/container', methods=['POST', 'GET'])
+def container_interactions():
+    if request.method == 'POST':
+        # TODO: create a new machine
+        return jenv.get_template("server-list-item.html").render(
+            servername="example-server-1",
+            status="online",
+            link="/app/1",
+            buttonstyle="background-color:green;",
+            containerid="999888999"
+        )
+    elif request.method =='GET':
+        # TODO: fetch from list of running machines
+        # TODO: obviously secure this to only be owned machines by that user
+
+        example_machines = [
+            {
+                'name': "example-server-2",
+                'status': "offline",
+                'id': "88877776666",
+                'index': "2"
+            },
+            {
+                'name': "example-server-3",
+                'status': "offline",
+                'id': "1686516156",
+                'index': "3"
+            },
+            {
+                'name': "example-server-4",
+                'status': "offline",
+                'id': "46464646",
+                'index': "4"
+            }
+        ]
+
+        output = ""
+        for example_machine in example_machines:
+            output += jenv.get_template("server-list-item.html").render(
+            servername=example_machine['name'],
+            status=example_machine['status'],
+            link="/app/" + example_machine['index'],
+            buttonstyle="",
+            containerid=example_machine['id']
+        )
+        return output
+
+    else:
+        return "err"
+
 if __name__ == '__main__':
     app.run()
 
