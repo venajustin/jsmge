6568899d0d206c8407ab36c2023d4e9bfa6da04f
venajustin <venajustin@gmail.com>
2025-03-21 10:42:30 -0700

    aditional changes to server output container

--- app-start/test.js
+++ app-start/test.js
@@ -1,5 +1,12 @@
 export function testfn() {
-    return "<form action='/status'>" +
+    return "<form onsubmit='getStatus()'>" +
         "<button type='submit' >ClickMe</button>" +
         "</form>";
 }
+
+export function getStatus(e) {
+    e.preventDefault();
+    let status = fetch("/status");
+    alert("Status: " + status);
+}
+
