9c90023a4d93a83faedee5dcafc7420f946cd604
venajustin <venajustin@gmail.com>
2025-04-23 15:15:58 -0700

    switch root to be localhost/root/

--- editor/src/App.jsx
+++ editor/src/App.jsx
@@ -23,8 +23,8 @@ function App() {
         <Link to="/">Home</Link> | <Link to="/editor">Editor</Link>
       </nav> */}
       <Routes>
-        <Route path="/engine/" element={<Home />} />
-        <Route path="/engine/editor" element={<Editor />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/editor" element={<Editor />} />
       </Routes>
     </Router>
   )
--- editor/vite.config.js
+++ editor/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/engine/",
   plugins: [react()],
   server: {
     allowedHosts: ["editor-container"],
