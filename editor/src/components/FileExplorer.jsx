aa6f0fc3b130fe908323657cd2a0da5fecc9da80
GreenXDShadow <NickDValen@gmail.com>
2025-04-30 23:03:59 -0700

    File explorer works.
    New rdependencies added in package.json so run npm install again
    
    https://dgreene1.github.io/react-accessible-treeview/docs/examples-MultiSelectDirectoryTree

---
+++ editor/src/components/FileExplorer.jsx
@@ -0,0 +1,94 @@
+// File Explorer Component goes here (NOT an entire webpage)
+// Default import from https://dgreene1.github.io/react-accessible-treeview/docs/examples-MultiSelectDirectoryTree
+import React from "react";
+import { DiCss3, DiJavascript, DiNpm } from "react-icons/di";
+import { FaList, FaRegFolder, FaRegFolderOpen } from "react-icons/fa";
+import TreeView, { flattenTree } from "react-accessible-treeview";
+import "../css/fileExplorer.css";
+
+const folder = {
+  name: "",
+  children: [
+    {
+      name: "This is a test file",
+      children: [{ name: "indexTester.js" }, { name: "stylesTester.css" }],
+    },
+    {
+      name: "This is my second folder",
+      children: [
+        {
+          name: "react-accessible-treeview",
+          children: [{ name: "index.js" }],
+        },
+        { name: "react", children: [{ name: "index.js" }] },
+      ],
+    },
+    {
+      name: ".npmignore",
+    },
+    {
+      name: "package.json",
+    },
+    {
+      name: "webpack.config.js",
+    },
+  ],
+};
+
+const data = flattenTree(folder);
+function MultiSelectDirectoryTreeView() {
+  return (
+    <div>
+      <div className="ide">
+        <TreeView
+          data={data}
+          aria-label="directory tree"
+          togglableSelect
+          clickAction="EXCLUSIVE_SELECT"
+          multiSelect
+          nodeRenderer={({
+            element,
+            isBranch,
+            isExpanded,
+            getNodeProps,
+            level,
+            handleSelect,
+          }) => (
+            <div {...getNodeProps()} style={{ paddingLeft: 20 * (level - 1) }}>
+              {isBranch ? (
+                <FolderIcon isOpen={isExpanded} />
+              ) : (
+                <FileIcon filename={element.name} />
+              )}
+              {element.name}
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  );
+}
+
+const FolderIcon = ({ isOpen }) =>
+  isOpen ? (
+    <FaRegFolderOpen color="e8a87c" className="icon" />
+  ) : (
+    <FaRegFolder color="e8a87c" className="icon" />
+  );
+
+const FileIcon = ({ filename }) => {
+  const extension = filename.slice(filename.lastIndexOf(".") + 1);
+  switch (extension) {
+    case "js":
+      return <DiJavascript color="yellow" className="icon" />;
+    case "css":
+      return <DiCss3 color="turquoise" className="icon" />;
+    case "json":
+      return <FaList color="yellow" className="icon" />;
+    case "npmignore":
+      return <DiNpm color="red" className="icon" />;
+    default:
+      return null;
+  }
+};
+export default MultiSelectDirectoryTreeView;
--- editor/src/pages/Editor.jsx
+++ editor/src/pages/Editor.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import '../css/editor.css';
 import CodeEditor from "../components/codeEditor";
+import FileExplorer from "../components/FileExplorer.jsx";
 
 
 
@@ -11,6 +12,9 @@ const Editor = () => {
     <div>
       <h2>Editor</h2>
       <CodeEditor/>
+      <h2>File Selector</h2>
+      <FileExplorer/>
+      {/* Insert FileExplorer component here */}
     </div>
   );
 };
