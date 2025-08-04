e0d1280aaf375356d033afcdde9470a914bb4ae2
GreenXDShadow <NickDValen@gmail.com>
2025-08-03 18:46:08 -0700

    codeEditor.jsx   -fixed typo editor.css   -editor.container had two instantiations, consolidated to one   -added .game-preview container to fix to top right corner   -added .left-pane to accomodate for the new game container   -added .right-pane to hold game preview and file editor window Editor.jsx   -Rewrote implementation to accomodate new CSS index.css   -I added a height and width req for iframe but in hindsight i dont think this did anything. .. gonna commit to be safe

--- editor/src/pages/Editor.jsx
+++ editor/src/pages/Editor.jsx
@@ -10,11 +10,21 @@ import GamePreview from "../components/GamePreview.jsx";
 const Editor = () => {
   return (
     <div className="horiz-resize-container">
-        {/* Left pane - takes remaining space */}
+        {/* Left pane - just the code editor */}
         <div className="left-pane">
             <div className="editor-container">
                 <CodeEditor/>
             </div>
+        </div>
+
+        {/* Right pane - game preview on top, file browser/properties below */}
+        <div className="right-pane">
+            {/* Game preview - fixed to top right */}
+            <div className="game-preview-container">
+                <GamePreview/>
+            </div>
+
+            {/* File browser and properties below game preview */}
             <div className="lower-right-container">
                 <div className="file-browser-container">
                     <FileExplorer/>
@@ -25,11 +35,6 @@ const Editor = () => {
                    </div>
                 </div>
             </div>
-        </div>
-
-        {/* Game preview - fixed to top right */}
-        <div className="game-preview-container">
-            <GamePreview/>
         </div>
     </div>
   );
