b660d90545127ef2e2f0840f4d52acaf60faecdf
venajustin <venajustin@gmail.com>
2025-08-07 23:51:03 -0700

    setup of game object and simple player connection

--- server-output/index.js
+++ server-output/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import path from "path";
 import * as fs from 'node:fs';
 
+// Change this to the directory loaded as usercode, set it to test-usercode for testing purposes
+const user_code_dir = "./test-usrcode";
+
 import { createServer } from "node:http";
 import  { Server } from "socket.io";
 
 import crypto from "crypto";
 import session from "express-session";
 
-import {getStatus, testfn} from '../usrcode/test.js';
+import {createGame, GameState} from './server-core/game.js';
+
+// import {} from '../usrcode/test.js';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,8 +22,12 @@ const port = process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server);
 
+const game = createGame();
 
 app.use(express.static('static'));
+app.use("/static", express.static("static"));
+app.use("/user-static", express.static(user_code_dir + "/resources"));
+
 
 const sessionMiddleware = session({
     secret: crypto.randomBytes(64).toString('hex'),
@@ -56,8 +66,9 @@ app.get("/test/:inputnum", (req, res) => {
 });
 
 app.get("/status", (req, res) => {
-    res.send("running");
+    res.send(game.state);
 });
+
 
 app.get("/files", (req, res) => {
   const folderPath = "./applications/1";
@@ -87,12 +98,28 @@ app.get('/favicon.ico', (req, res) => {
 io.on('connection', (socket) => {
     const sessionId = socket.request.session.id;
     socket.join(sessionId);
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-        io.to(sessionId).emit('chat message', "you just sent this ^ ");
-        console.log(msg);
+
+    if (game.state === GameState.EDIT) {
+        io.to(sessionId).emit('game_status', "edit");
+    }
+    // io.emit('chat message', "Player " + sessionId + " session established");
+    console.log("Session " + sessionId + " established");
+
+    game.players.push(sessionId);
+
+    socket.on('disconnect',() => {
+       game.players.splice(game.players.indexOf(sessionId),1);
+       console.log("Session " + sessionId + " disconnected");
+       console.log("Active Sessions: " + game.players.length);
     });
+    // chat room test:
+    // socket.on('chat message', (msg) => {
+    //     io.emit('chat message', msg);
+    //     io.to(sessionId).emit('chat message', "you just sent this ^ ");
+    //     console.log(msg);
+    // });
 });
+
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
---
+++ server-output/static/run_client.js
@@ -0,0 +1,3 @@
+setupCanvas();
+
+update();
