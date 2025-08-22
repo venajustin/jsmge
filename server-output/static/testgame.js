1c8f78dbca448f44244e6f954abf058ab9247427
Justin Greatorex <venajustin@gmail.com>
2025-08-21 17:10:04 -0700

    Frame class

--- server-output/static/testgame.js
+++ server-output/static/testgame.js
@@ -1,9 +1,58 @@
+// let frameModule;
+// possibly conditional import depending on server/client 
+// aka ` if (typeof window === "undefined") { `
+
+// frameModule =  import("./core/frame.js");
+// import("./core/frame.js").then((frameMod) => {
+
+
+let events = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12"
+]
+
 function setup() {
     createCanvas(1600, 1200, P2D, document.getElementById('display-canvas'));
 
+    obj1 = new Frame();
+    obj1.position = {x:500,y:500,z:0};
+    obj1.childrenObj.push(new Frame());
+    obj1.childrenObj.push(new Frame());
+    obj1.childrenObj[0].position = {x:30,y:30,z:0};
+    obj1.childrenObj[1].position = {x:-30,y:30,z:0};
+}
+
+function keyPressed() {
+    if (key === 'a') {
+        console.log("a pressed");
+        let newnum = 0;
+        events.forEach((num) => {
+            if (num >= newnum) {
+                newnum = parseint(num) + 1;
+            }
+        });
+        console.log("newnum: " + newnum);
+        events.push(newnum.toString());
+    }
 }
 
 function draw() {
-    circle(100,100,100);
-     
+    textSize(30);
+    text("hi", 100,100);
+    obj1.draw();
+
 }
+
+
+
+
