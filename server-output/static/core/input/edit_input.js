662b91b8b773aea95a38157d52fa4d3b0b12222a
venajustin <venajustin@gmail.com>
2025-09-01 11:22:43 -0700

    draggable x/y bars and render simple image by index

--- server-output/static/core/input/edit_input.js
+++ server-output/static/core/input/edit_input.js
@@ -7,8 +7,10 @@ function process_edit_input(editState) {
 }
 
 function edit_mouse_press(editState) {
+    editState.lockX = false;
+    editState.lockY = false;
     const point = {x:mouseX, y:mouseY};
-    editState.selected = scene._intersect_point(point);
+    editState.selected = scene._edit_drag_intersect(point, editState);
     editState.dragLast = point;
 }
 
@@ -16,10 +18,30 @@ function edit_mouse_drag(editState) {
     const point = {x:mouseX, y:mouseY};
     const dx = point.x - editState.dragLast.x;
     const dy = point.y - editState.dragLast.y;
-    editState.selected.forEach((o) => {
-       o.position.x += dx;
-       o.position.y += dy;
-    });
+    if (editState.selected.length > 0) {
+        // TODO: clean up this functionality
+        // I want it to only move one object, but i want that object to be the lowest in the chain of ownership
+        // That way when objects overlap you can seperate them
+
+        const last = editState.selected.length - 1;
+        editState.selected[last]._pos.x += dx;
+        editState.selected[last]._pos.y += dy;
+    }
+    // editState.selected.forEach((o) => {
+    //    if (!editState.lockX) {
+    //        o._pos.x += dx;
+    //    }
+    //    if (!editState.lockY) {
+    //        o._pos.y += dy;
+    //    }
+    // });
+
+    // if (editState.selected.size > 0) {
+    //     const obj = editState.selected[0];
+    //     obj.pos.x += dx;
+    //     o._pos.y += dy;
+    // }
+
     editState.dragLast = point;
 }
 
