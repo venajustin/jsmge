c11233267728cdc82c1ffe3d2601d254a7820403
KunalPradhan1 <kunal3421@gmail.com>
2025-09-03 14:34:52 -0700

    final changes from kunal on this branch before making new one

---
+++ static/js/login.js
@@ -0,0 +1,32 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("form");
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const formData = new FormData(loginForm);
+        
+
+        try {
+            const response = await fetch("/login", {
+                method: "POST",
+                body : formData,
+            });
+            const data = await response.json();
+            if(response.ok){
+                localStorage.setItem("token", data.token);
+                console.log("Token saved:", data.token);
+
+
+                alert("Login successful");
+                window.location.href = "/";
+            } else {
+                alert(data.message || "Login failed");
+            }
+        } catch (error) {
+            console.error("error during login:", error);
+            alert("An error occured. Please try again");
+        
+        }
+    });
+});
