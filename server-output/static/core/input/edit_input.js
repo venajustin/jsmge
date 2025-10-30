f8c224a792e5e9050611823d292f8d001cb46f48
Justin Greatorex <venajustin@gmail.com>
2025-10-30 16:25:56 -0700

    <untested> make selected rely on bool in editState rather than length of list

--- server-output/static/core/input/edit_input.js
+++ server-output/static/core/input/edit_input.js
@@ -10,15 +10,27 @@ export function edit_mouse_press(p, editState) {
     editState.lockX = false;
     editState.lockY = false;
     const point = {x:p.mouseX, y:p.mouseY};
+    editState.currently_holding = false;
     editState.selected = p.scene._edit_drag_intersect(point, editState);
+    if (editState.selected.length > 0) {
+        editState.currently_holding = true;
+    }
     editState.dragLast = point;
+
+
+    //  At this point, send socket packet to server containing sel._id
+    //  const last = editState.selected.length - 1;
+    // const sel = editState.selected[last];
+    
+
+
 }
 
 export function edit_mouse_drag(p, editState) {
     const point = {x:p.mouseX, y:p.mouseY};
     const dx = point.x - editState.dragLast.x;
     const dy = point.y - editState.dragLast.y;
-    if (editState.selected.length > 0) {
+    if (editState.currently_holding) {
         // TODO: clean up this functionality
         // I want it to only move one object, but i want that object to be the lowest in the chain of ownership
         // That way when objects overlap you can seperate them
@@ -50,7 +62,8 @@ export function edit_mouse_drag(p, editState) {
 }
 
 export function edit_mouse_click(p, editState) {
-    editState.selected = undefined; //will remove this line to put like .isSelected = false
+    // editState.selected = undefined; //will remove this line to put like .isSelected = false
+    editState.currently_holding = false;
 }
 
 
